b71e94573bd4a3fd6db11a86c81d9a1e95c8fd88
ZeengFong <lsuanez@icloud.com>
2025-03-14 11:06:53 -0600

    ts pmo sybau </3

---
+++ oop/linkedlists/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        while (true){
+            System.out.println("pmopmopmo  pmopmopmo\n   pmo     pmo      \n   pmo     pmopmopmo\n   pmo           pmo\n   pmo     pmopmopmo\n");
+            Thread.sleep(400);
+            System.out.println("tstststs  tsts tsts   tstststs\nts    ts  ts  ts  ts  ts    ts\ntstststs  ts  ts  ts  ts    ts\nts        ts  ts  ts  ts    ts\nts        ts  ts  ts  tstststs\n");
+            Thread.sleep(400);
+            System.out.println("pmopmopmo  pmo   pmo  pmopmopmo  pmopmopmo  pmo   pmo\npmo        pmo   pmo  pmo   pmo  pmo   pmo  pmo   pmo\npmopmopmo  pmopmopmo  pmopmopmo  pmopmopmo  pmo   pmo\n      pmo     pmo     pmo   pmo  pmo   pmo  pmo   pmo\npmopmopmo     pmo     pmopmopmo  pmo   pmo  pmopmopmo\n");
+            Thread.sleep(400);
+            System.out.println("  tsts  tsts  \ntsts  tstststs\n tststs  tsts\n  tsts  tsts  \n     ts ts    \n");
+            Thread.sleep(400);
+        }
+    }
+}
--- oop/oop/src/Main.java
+++ oop/oop/src/Main.java
@@ -5,11 +5,15 @@ public class Main {
 
     public static int birthday(Human h) {
         h.setAge(h.getAge() + 1);
+        System.out.println("happy brithday " + h.getName());
         return h.getAge();
     }
 
     public static void main(String[] args) {
         Human Luca = new Human();
         System.out.println(checkName(Luca));
+        System.out.println(Luca.getAge());
+        birthday(Luca);
+        System.out.println(Luca.getAge());
     }
 }
